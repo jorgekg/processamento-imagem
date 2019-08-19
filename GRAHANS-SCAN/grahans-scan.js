702436295b859345b598ec953f8a0501e9cdb51b
jorgekg <jorgekg3@gmail.com>
2019-08-19 18:38:19 -0300

    final

--- GRAHANS-SCAN/grahans-scan.js
+++ GRAHANS-SCAN/grahans-scan.js
@@ -8,16 +8,24 @@ const ccw = (ponto1, ponto2, ponto3) => {
 
 // adiciona 3 itens a lista e verifica se náo houve curva
 // caso houver remove o length - 2 item.
-const logica = (listaOrdenada) => {
+const logica = (listaOrdenada, isReverse = false) => {
   const lista = [];
   for (let i = 0; i < listaOrdenada.length; i++) {
     lista.push(listaOrdenada[i]);
     while (lista.length > 2) {
       const [ponto3, ponto2, ponto1] = Object.assign([], lista).reverse();
-      if (ccw(ponto1, ponto2, ponto3)) {
-        lista.splice((lista.length - 2), 1);
+      if (isReverse) {
+        if (!ccw(ponto1, ponto2, ponto3)) {
+          lista.splice((lista.length - 2), 1);
+        } else {
+          break;
+        }
       } else {
-        break;
+        if (ccw(ponto1, ponto2, ponto3)) {
+          lista.splice((lista.length - 2), 1);
+        } else {
+          break;
+        }
       }
     }
   }
@@ -25,8 +33,25 @@ const logica = (listaOrdenada) => {
 }
 
 // Verifica se a lista é par, sendo par é concavo
-const isConcavo = (list) => {
-  return list.length & 1 ? 'Convexo' : 'Concavo';
+const calcularConcavoOuConvexo = (listaX, listaY) => {
+  let isConvexo = false;
+  let valX = 0;
+  let valY = 0;
+  listaX.forEach(x => {
+    if (x.x >= valX) {
+      valX = x.x;
+    } else {
+      isConvexo = true;
+    }
+  })
+  listaY.forEach(y => {
+    if (y.y >= valY) {
+      valY = y.y;
+    } else {
+      isConvexo = true;
+    }
+  })
+  return isConvexo ? 'convexo': 'concavo';
 }
 
 // lista com os dados em 2d dos pontos.
@@ -70,5 +95,10 @@ listaDePontosSuperiores.forEach(inf01 => {
 
 console.log(listaPontosInferior);
 
+const listaDePontos = listaDePontosSuperiores.concat(listaPontosInferior);
+
+const listaDePontosOrdenadoEmX = Object.assign([], listaDePontos).sort((a, b) => a.x - b.x);
+const listaDePontosOrdenadoEmY = Object.assign([], listaDePontos).sort((a, b) => a.y - b.y);
+
 // Mostra se é concavo ou convexo.
-console.log(isConcavo(listaDePontosSuperiores.concat(listaPontosInferior)));
+console.log(calcularConcavoOuConvexo(listaDePontosOrdenadoEmX, listaDePontosOrdenadoEmY));
