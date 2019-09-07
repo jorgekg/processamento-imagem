beed43915deabf24407155a0564049307e65b68b
Jorge Guilherme <jorgeguilherme@MacBook-Pro-de-Jorge.local>
2019-09-07 10:31:09 -0300

    com

--- morphological-enhancement/__init__.py
+++ morphological-enhancement/__init__.py
@@ -1,35 +1,28 @@
 import cv2 
 import numpy as np
 
-# read image of disk
 img = cv2.imread('images/image.jpg')
 
-# transform imagem to gray scale
+cv2.imshow('image', img)
+cv2.waitKey(5000)
+
 image_gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# generate kernel
 kernel = np.ones((5, 5), np.uint8)
 
-# generate erosion
-image_erosion = cv2.erode(image_gray_scale, kernel, iterations=1)
+opening = cv2.morphologyEx(image_gray_scale, cv2.MORPH_OPEN, kernel)
 
-# generate opening
-opening = cv2.dilate(image_erosion, kernel, iterations=1)
+top_hat_opening = cv2.subtract(image_gray_scale, opening)
 
-# generate top hat
-tap_hat_opening = cv2.subtract(img, opening)
+closing = cv2.morphologyEx(image_gray_scale, cv2.MORPH_CLOSE, kernel)
 
-# generate dilatetion
-image_dilated = cv2.dilate(image_gray_scale, kernel, iterations=1)
+top_hat_closing = cv2.subtract(closing, image_gray_scale)
 
-# generate closing
-closing = cv2.erode(image, kernel, iterations=1)
+im = cv2.add(image_gray_scale, top_hat_opening)
 
-# generate top hat closing
-tap_hat_closing = cv2.subtract(closing, img)
+k = cv2.subtract(im, top_hat_closing)
 
-k = cv2.subtract(cv2.add(img,tap_hat_opening), tap_hat_closing)
-
-(status, image) = cv2.threshold(k, 100, 255, cv2.THRESH_BINARY)
+(status, image) = cv2.threshold(k, 230, 255, cv2.THRESH_BINARY)
 
 cv2.imshow('image', image)
+cv2.waitKey()
