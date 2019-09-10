dd087b26467e5d6c3d1d0f7720557c3a5474a24b
Jorge Guilherme <jorgeguilherme@MacBook-Pro-de-Jorge.local>
2019-09-09 21:20:05 -0300

    filter red

---
+++ filter-red/__init__.py
@@ -0,0 +1,30 @@
+import cv2
+
+# get camera object
+cam = cv2.VideoCapture(0)
+
+while True:
+    # get camera stream
+    status, image = cam.read()
+
+    if (status):
+        # camera not working
+        break
+    
+    # get canal of imagens
+    b, g, r = cv2.split(image)
+
+    # apply threshold
+    (status, img) = cv2.threshold(r, 100, 255, cv2.THRESH_BINARY_INV)
+
+    # show image;
+    cv2.imshow("image", img)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+# release camera
+
+cam.release()
+
+cv2.destroyAllWindows()
